4c4d6d56984e63220c084c2f95b49319d073031c
Neurrone <none>
2016-03-20 16:32:43 +0800

    Implemented mutation (gaussian convolution algorithm) and fitness functions.

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -4,10 +4,17 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class GeneticAlgorithm {
 public int populationSize;
+public float crossoverRate; 
+public static final int NUM_GAMES = 5; // number of games to run to assess fitness of an individual
 ArrayList<ArrayList<FeatureWeightPair>> population;
+PlayerSkeleton player;
+private Random rng;
 
 public GeneticAlgorithm(int populationSize) {
+	rng = new Random();
+	player = new PlayerSkeleton();
 	population = new ArrayList<ArrayList<FeatureWeightPair>>(populationSize);
+	crossoverRate = 1.0f / populationSize;
 	
 	for (int i = 0; i < populationSize; i++) {
 		population.add(generateRandomIndividual());
@@ -35,10 +42,64 @@ private ArrayList<FeatureWeightPair> generateRandomIndividual() {
 
 	return individual;
 }
-
+// returns a new float in the range [min, max)
 private float randomFloat(float minInclusive, float maxExclusive) {
  return (float) ThreadLocalRandom.current().nextDouble(minInclusive, maxExclusive);
 }
+
+// incomplete
+public void train(int generations) {
+	assert(generations > 0);
+	int bestIndex = 0;
+FitnessAssessment bestFitness = assessFitness(population.get(0));
+
+for (int individual = 1; individual < population.size(); individual++) {
+	FitnessAssessment fitness = assessFitness(population.get(individual));
+	
+	if (bestFitness.compareTo(fitness)  > 0) {
+		bestFitness = fitness;
+		bestIndex = individual;
+	}
+}
+
+
+}
+
+// returns information about the lowest, average and highest score on an individual after playing NUM_GAMES games, each game with random piece sequences
+private FitnessAssessment assessFitness(ArrayList<FeatureWeightPair> individual) {
+	player.setFeatureWeightPairs(individual);
+	int lowest = 0, highest = 0, total = 0;
+	
+	for (int game = 0; game < NUM_GAMES; game++) {
+		int score = player.playGame(false);
+		total += score;
+		
+		lowest = Math.min(lowest, score);
+		highest = Math.max(highest, score);
+	}
+	
+	float average = total * 1.0f / NUM_GAMES;
+	return new FitnessAssessment(individual, lowest, average, highest);
+}
+
+// crosses over 2 individuals, not implemented 
+private void crossover(ArrayList<FeatureWeightPair> x, ArrayList<FeatureWeightPair> y) {
+
+}
+
+// mutates an individual using the Gaussian Convolution algorithm
+private void mutate(ArrayList<FeatureWeightPair> individual) {
+for (int i = 0; i < individual.size(); i++) {
+	float n = 0.0f;
+	
+	do {
+		n  = (float) rng.nextGaussian(); // this uses standard dev of 1.0, not sure if this needs to be changed later
+	} while (individual.get(i).weight + n >= 0.0f);
+		
+		individual.get(i).weight += n;
+}
+
+	}
 
 	public static void main(String[] args) {
 		GeneticAlgorithm ga = new GeneticAlgorithm(100);
@@ -62,4 +123,43 @@ private float randomFloat(float minInclusive, float maxExclusive) {
 
 	}
 
+	// stores information about the fitness of an individual
+	public static class FitnessAssessment implements Comparable<FitnessAssessment> {
+		public ArrayList<FeatureWeightPair> individual;
+		public int lowest, highest; // scores
+		public float average; // average score
+		
+		public FitnessAssessment(ArrayList<FeatureWeightPair> individual, int l, float a, int h) {
+			this.individual = individual;
+			lowest = l;
+			average = a;
+			highest = h;
+		}
+		
+		public int compareTo(FitnessAssessment other) {
+		if (other.average < average)
+			return -1;
+		
+		if (other.average > average)
+			return 1;
+		
+		// tiebreak using lowest
+		if (other.lowest < lowest)
+			return -1;
+		
+		if (other.lowest > lowest)
+			return 1;
+		
+		// then highest
+		if (other.highest < highest)
+			return -1;
+		
+		if (other.highest > highest)
+			return 1;
+		
+		return 0;
+		}
+	}
+
+	
 }
