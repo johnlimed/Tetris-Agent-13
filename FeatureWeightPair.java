e65dccbfa260d98c99d3f41bf219a4ea21291a1e
John Lim <johnlimed@gmail.com>
2016-03-23 12:26:31 +0800

    indented more files

--- FeatureWeightPair.java
+++ FeatureWeightPair.java
@@ -1,11 +1,11 @@
 // a convenience function to represent a feature and its associated weight
 
 public class FeatureWeightPair {
-public FeatureFunction feature;
-public float weight;
+	public FeatureFunction feature;
+	public float weight;
 
-FeatureWeightPair(FeatureFunction f, float w) {
-	feature = f;
-	weight = w;
-}
+	FeatureWeightPair(FeatureFunction f, float w) {
+		feature = f;
+		weight = w;
+	}
 }
--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -6,42 +6,42 @@ public class PlayerSkeleton {
 	public static final int COLS = 10;
 	public static final int ROWS = 21;
 	public static final int N_PIECES = 7;
-private ArrayList<FeatureWeightPair> features;
+	private ArrayList<FeatureWeightPair> features;
 
 
 	public PlayerSkeleton() {
 		features = new ArrayList<FeatureWeightPair>();
 
 	}
-	
+
 
 	public void setFeatureWeightPairs(ArrayList<FeatureWeightPair> features) {
 		this.features = features;
 	}
-	
+
 	// returns  h(n) for the given state
 	private float evaluate(ImprovedState s) {
 		float sum = 0.0f;
-		
+
 		for (int i=0; i<features.size(); i++) {
 			FeatureWeightPair f = features.get(i);
 			sum += f.feature.evaluate(s) * f.weight;
 		}
-			
-			return sum;
+
+		return sum;
 	}
-	
+
 	//implement this function to have a working system
 	public int pickMove(State s, int[][] legalMoves) {
 		// s is the current state
 		// legalMoves is legalMoves for next piece, 2D array [numLegalMoves][0 = orient/ 1 = slot]
-		
+
 		ImprovedState currentState = new ImprovedState(s);
 
 		int bestMove = 0;
 		boolean isNonLosingMoveFound = false;
 		float bestValue = 0.0f;
-		
+
 		// find the first legal move corresponding to a non-losing situation, and assume that to be the best move
 		for (int move = 0; move < legalMoves.length && isNonLosingMoveFound == false; move++) {
 			ImprovedState resultingState = currentState.tryMove(move);
@@ -52,23 +52,23 @@ private ArrayList<FeatureWeightPair> features;
 				bestValue = evaluate(resultingState);
 			}
 		}
-			
+
 		if (isNonLosingMoveFound == false)
 			return 0; // if we'll die anyway, it doesn't matter which move we do
-		
+
 		// now see if we can find better moves
 		for (int move = bestMove; move < legalMoves.length; move++) {
 			ImprovedState resultingState = currentState.tryMove(move);
 			if (resultingState.hasLost() == false) {
-		float utility = evaluate(resultingState);	
-		
-		if (utility > bestValue) {
-			bestValue = utility;
-			bestMove = move;
-		}
+				float utility = evaluate(resultingState);
+
+				if (utility > bestValue) {
+					bestValue = utility;
+					bestMove = move;
+				}
 			}
 		}
-		
+
 		return bestMove;
 
 		/* 
@@ -91,47 +91,47 @@ private ArrayList<FeatureWeightPair> features;
 			System.out.println("legalMoves[][] length: " + legalMoves[i].length);
 		}
 		*/
-		
+
 // 		return randMove; // return moveNumber in legalMoves[moveNum][orient/slot]
 	}
-	
+
 	// plays a game , returning the number of rows completed
 	// use the setFeatureWeightPairs function first
 	public int playGame(boolean draw) {
 		assert (features.isEmpty() == false); // must set some features to use first
 		State s = new State();
-		
+
 		if (draw)
 			new TFrame(s);
 
 		while(!s.hasLost()) {
 			s.makeMove(pickMove(s,s.legalMoves()));
-			
+
 			if (draw) {
-			s.draw();
-			s.drawNext(0,0);
-			
-			try {
-				Thread.sleep(100);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				s.draw();
+				s.drawNext(0,0);
+
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
-			}
-			
+
 		}
-	
+
 		return s.getRowsCleared();
 	}
-	
+
 	public static void main(String[] args) {
 		PlayerSkeleton p = new PlayerSkeleton();
 		// these weights are from a test run with 2 generations of the GA
 		ArrayList<FeatureWeightPair> fwPairs = new ArrayList<FeatureWeightPair>();
-fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.AggHeight(), -1.5823991f));
+		fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.AggHeight(), -1.5823991f));
 		fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.Bumpiness(), -0.33376628f));
 		fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.MaxHeight(), -0.19359511f));
 		fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.NumHoles(), -0.23551378f));
-			p.setFeatureWeightPairs(fwPairs);	
+		p.setFeatureWeightPairs(fwPairs);
 		System.out.println("You have completed "+p.playGame(true) +" rows.");
 		/* 
 		State s = new State();
@@ -149,10 +149,10 @@ fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.AggHeight(), -1.5823991f));
 		}
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
 		*/
-		
+
 	}
 
-		
+
 	// function to return numCompleteLines: unsure
 	private static int numCompleteLines(State s) {
 		return s.getRowsCleared(); // this gets the cumulative total number of rows cleared so far. Do we want this? 
@@ -162,25 +162,25 @@ fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.AggHeight(), -1.5823991f));
 
 
 	// returns aggregate height for all columns
-			public static class AggHeight implements FeatureFunction {
-				@Override
+	public static class AggHeight implements FeatureFunction {
+		@Override
 		public float evaluate(ImprovedState s)  {
-				int aggHeight = 0;
+			int aggHeight = 0;
 
-				int[] top = s.getTop();
+			int[] top = s.getTop();
 
-				for (int i = 0; i < State.COLS; i++) {
-					aggHeight += top[i];
-				}
-
-				return (float) aggHeight;
+			for (int i = 0; i < State.COLS; i++) {
+				aggHeight += top[i];
 			}
-			}
-			
-			// returns number of holes. A hole is an empty space such that there is at least one tile in the same column above it
+
+			return (float) aggHeight;
+		}
+	}
+
+	// returns number of holes. A hole is an empty space such that there is at least one tile in the same column above it
 	public static class NumHoles implements FeatureFunction {
 		@Override
-		public float evaluate(ImprovedState s) {	
+		public float evaluate(ImprovedState s) {
 			int numHoles = 0;
 
 			int[][] field = s.getField();
@@ -215,7 +215,7 @@ fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.AggHeight(), -1.5823991f));
 		}
 	}
 
-		// maximum column height heuristic: DONE
+	// maximum column height heuristic: DONE
 	public static class MaxHeight implements FeatureFunction {
 		@Override
 		public float evaluate(ImprovedState s) {
@@ -233,6 +233,6 @@ fwPairs.add(new FeatureWeightPair(new PlayerSkeleton.AggHeight(), -1.5823991f));
 			return maxColumnHeight;
 		}
 	}
-		
+
 }
 
